8a06f78c534ccbdc0cc8a67b5b06cbf3c82bafdc
Emmanuel-Aggrey <aggrey.en@gmail.com>
2020-06-23 03:35:46 +0000

    .

--- shortend_url/serializers.py
+++ shortend_url/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import  serializers
 from .models import  Link
 
 class LinkSerializers(serializers.ModelSerializer):
-   
+    # shortened_url = serializers.URLField(source='short_url')
     class Meta:
         model = Link
         fields = ['id','shortened_url','original_url']
